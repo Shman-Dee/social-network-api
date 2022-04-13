f3b07064b28fdc554051d5ed8b82ca802348c362
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-12 23:36:24 -0600

    added models

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+const moment = require("moment");
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+
+    username: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false, //don't return the id of the emlement(s)
+  }
+);
+// Create a virtual property `reactionCount` that gets the amount of reactions per thought
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      //regular expression to check if it's a valid email address
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `friendCount` that gets the amount of reactions per user
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
